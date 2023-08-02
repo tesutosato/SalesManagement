c1d125efe8b69c1026ad048c24452ccf2dbd13a5
tesutosato <sachitesuto22@gmail.com>
2023-08-03 00:38:38 +0900

    SalesSeederの作成、DBへの反映

---
+++ database/seeders/SalesSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class SalesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('sales')->insert([
+            [
+                'product_id' => '1',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+            ],
+            [
+                'product_id' => '2',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+            ],
+            [
+                'product_id' => '3',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+            ],
+            [
+                'product_id' => '4',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+            ],
+            [
+                'product_id' => '5',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+            ],
+            [
+                'product_id' => '6',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+            ],
+            [
+                'product_id' => '7',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+            ],
+            [
+                'product_id' => '8',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+            ],
+            [
+                'product_id' => '9',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+            ],
+            [
+                'product_id' => '10',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => null,
+            ],
+        ]);
+    }
+}
